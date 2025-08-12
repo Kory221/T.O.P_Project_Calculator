56b27fd53dc0267b500fe23a5e563812a59c2fdd
Kory <200974544+Kory221@users.noreply.github.com>
2025-08-12 19:07:12 +0200

    HTML Elements + clickEventListeners
    Added some buttons and a display on HTML for a basic calculator.
    Some ClickEventListeners to display the operation and store
    variables num1, num2 and operator (not finished yet).

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Final project of the Fundations part in The Odin Project cursus.
 In this project, we’re going to make an on-screen calculator using JavaScript, HTML, and CSS.
 
 1 - Create functions for add, subtract, multiply and divide.
+
 2- Create three variables, one for each part of the operation. 
 	You’ll use these variables to update your display later.
-3- 
+
+3- Create a new function called operate that takes an operator and two numbers 
+and then calls one of the above functions on the numbers.
+
 
 Link to the project on THE ODIN PROJECT website
 https://www.theodinproject.com/lessons/foundations-calculator
--- javascript.js
+++ javascript.js
@@ -15,9 +15,10 @@ function divide (a,b) {
     return a / b;
 }
 // 3 variable for operands and operator
-let num1;
-let num2;
-let operator;
+let num1='';
+let num2='';
+let operator='';
+// new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
 
 function operate (num1,num2,operator) {
     let result;
@@ -35,3 +36,30 @@ function operate (num1,num2,operator) {
     };
     return result;
 }
+//create the functions that populate the display when you click the digit buttons
+
+const display = document.querySelector(".display");
+
+const nums = document.querySelectorAll(".num");
+nums.forEach((num) => {
+    num.addEventListener("click", () => {
+        display.textContent += num.textContent;
+        num1 += num.textContent;
+    })
+});
+
+const opers = document.querySelectorAll(".oper");
+opers.forEach((oper) => {
+    oper.addEventListener("click", () => {
+        operator = oper.textContent;
+        display.textContent = display.textContent+' '+oper.textContent+' ';
+    })
+})
+
+// l'idée est que si on clique sur un opérateur, le premier
+//eventlistener sur les nums s'arrête et un autre 
+//commence faisant la même chose mais remplissant
+//cette fois num2.
+
+//ET aussi, si on clique sur 2 opérateurs (ou le même) de suite,
+//que seul le 2eme aparaisse (au lieu d'avoir +-) et =operator
