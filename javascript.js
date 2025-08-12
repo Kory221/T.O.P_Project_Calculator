ee3ec79a131aa8c3c42e17bf271f77d87f28e8f9
Kory <200974544+Kory221@users.noreply.github.com>
2025-08-12 17:53:56 +0200

    new function calling one of the 4 calc functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// functions for add, subtract, multiply and divide
 function add (a,b) {
     return a + b;
 }
@@ -13,7 +14,24 @@ function multiply (a,b) {
 function divide (a,b) {
     return a / b;
 }
-
-let firstOperand;
-let secondOperand;
+// 3 variable for operands and operator
+let num1;
+let num2;
 let operator;
+
+function operate (num1,num2,operator) {
+    let result;
+    if (operator === "+") {
+        result = add(num1,num2)
+    }
+    else if (operator === "-") {
+        result = subtract(num1,num2)
+    }
+    else if (operator === "*") {
+        result = multiply(num1,num2)
+    }
+    else if (operator === "/") {
+        result = divide(num1,num2)
+    };
+    return result;
+}
