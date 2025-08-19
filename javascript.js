d6ffafd2feedaa8f1aeb0238d749e87fbd93abc4
Kory <200974544+Kory221@users.noreply.github.com>
2025-08-19 17:43:14 +0200

    Store integer and decim part for each num
    So, during num.Event and clear.Event,
    if num includes('.') and decim start with '0',
    we can display it completely using Number(integ)+decim,
    what we couldn't do by just displaying Number(num) while
    writing the operation.
    Ex: before this change, if we want to write '12.01',
    while writing '12.0' it would display only '12' until
    we write fully '12.01'.

--- javascript.js
+++ javascript.js
@@ -41,15 +41,35 @@ let display = document.querySelector(".display");
 display.textContent = '0';
 
 const nums = document.querySelectorAll(".num");
+let integ1 = '';
+let decim1 = '';
+let integ2 = '';
+let decim2 = '';
 nums.forEach((num) => {
     num.addEventListener("click", () => {
         if (oper === '' && num1 !== result) {
+            if (num1.includes('.')){
+                num1 += num.textContent;
+                integ1 = num1.slice(0,num1.indexOf('.'));
+                decim1 = num1.slice(num1.indexOf('.'));
+                display.textContent = Number(integ1)+decim1;
+            }
+            else {
             num1 += num.textContent;
             display.textContent = Number(num1);
+            }
         }
         else if (oper !=='') {
+            if (num2.includes('.')) {
+                num2 += num.textContent;
+                integ2 = num2.slice(0,num2.indexOf('.'));
+                decim2 = num2.slice(num2.indexOf('.'));
+                display.textContent = Number(num1)+' '+oper+' '+Number(integ2)+decim2;
+            }
+            else {
             num2 += num.textContent;
             display.textContent = Number(num1)+' '+oper+' '+Number(num2);
+            }
         };
         
     })
@@ -137,6 +157,10 @@ clear.addEventListener("click", ()=> {
     num1 = '';
     num2 = '';
     oper = '';
+    integ1 = '';
+    decim1 = '';
+    integ2 = '';
+    decim2 = '';
     display.textContent = '0';
 });
 
@@ -144,8 +168,16 @@ clear.addEventListener("click", ()=> {
 const backsp = document.querySelector(".bsp");
 backsp.addEventListener("click", () => {
     if (num1!=='' && typeof num1 === 'string' && oper ==='' && num2 === '') {
-        num1 = num1.slice(0,-1);
-        display.textContent = Number(num1);
+        if (num1.includes('.') && num1.length>num1.indexOf('.')+1) {
+            num1 = num1.slice(0,-1);
+            integ1 = num1.slice(0,num1.indexOf('.'));
+            decim1 = num1.slice(num1.indexOf('.'));
+            display.textContent = Number(integ1)+decim1;
+        }
+        else {
+            num1 = num1.slice(0,-1);
+            display.textContent = Number(num1);
+        }
     }
     else if (num1!== '' && oper !=='' && num2==='') {
         oper = oper.slice(0,-1);
@@ -153,8 +185,16 @@ backsp.addEventListener("click", () => {
     }
     else if (num1!=='' && oper !== '' && num2 !== '' && typeof num2 === 'string') {
         if (num2.length > 1) {
+            if (num2.includes('.') & num2.length>num2.indexOf('.')+1) {
             num2 = num2.slice(0,-1);
-        display.textContent = Number(num1)+' '+oper+' '+Number(num2);
+            integ2 = num2.slice(0,num2.indexOf('.'));
+            decim2 = num2.slice(num2.indexOf('.'));
+            display.textContent = Number(num1)+' '+oper+' '+Number(integ2)+decim2;  
+            }
+            else {
+            num2 = num2.slice(0,-1);
+            display.textContent = Number(num1)+' '+oper+' '+Number(num2);
+            }
         }
         else {
             num2 = num2.slice(0,-1);
